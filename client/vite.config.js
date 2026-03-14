2479354410c4524384fa024754cf4697920b3a4a
Ahmadjon-swe <ahmadovahmadjonqsh125@gmail.com>
2026-03-14 14:46:40 +0500

    vite.config.js corrected

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4001', 
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        cookieDomainRewrite: 'localhost', // ✅ qo'shing
-      },  
-      '/uploads': {
         target: 'http://localhost:4001',
         changeOrigin: true,
-<<<<<<< HEAD
-=======
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        cookieDomainRewrite: 'localhost',
       },
       '/uploads': {
         target: 'http://localhost:4001',
         changeOrigin: true,
->>>>>>> 2f0ce334e2f27ff2e0ef23c2963c889b0c33b4e5
       }
     }
   }
